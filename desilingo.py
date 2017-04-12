1d73473af476e11812feaecb609c8b1a9fd35738
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-04-12 21:09:02 +0530

    added dataset dependency + add user url

--- desilingo.py
+++ desilingo.py
@@ -1,16 +1,38 @@
 import bottle
-from utils import render
+import utils
+import dataset
 
+
+render = utils.render
 app = bottle.Bottle()
+DB_STRING = 'sqlite:///desilingo.sqlite3'
 
 # ----------routes
 @app.get('/')
 def home():
     return render('home.html')
 
-@app.get('/<target>/<medium>')
-def learn_language(target, medium):
+@app.get('/<user>/<target>/<medium>/<level>/<item>')
+def learn_language(user, target, medium, level):
+    question = utils.get_question(level, item)
     return render('learn.html')
+
+@app.get('/<user>/add')
+def add_user(user):
+    report = None
+    with dataset.connect(DB_STRING) as db:
+        users = db['Users']
+        existing_user = users.find_none(nick=user)
+        if existing_user is None:
+            users.insert({'nick': user})
+            report = 'User added'
+        else:
+            report = 'User already exists'
+    return {'report': report}
+
+
+
+        
 
 if __name__ == '__main__':
     bottle.run(app, host='localhost', port=8080, debug=True)
