33b0b90ab5127373718143fc44c4fd7cc6b343e7
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-04-27 01:35:35 +0530

    added skeleton, Shas pic, home.html skeleton up

--- desilingo.py
+++ desilingo.py
@@ -12,27 +12,34 @@ DB_STRING = 'sqlite:///desilingo.sqlite3'
 def home():
     return render('home.html')
 
-@app.get('/<user>/<target>/<medium>/<level>/<item>')
-def learn_language(user, target, medium, level):
-    question = utils.get_question(level, item)
-    return render('learn.html')
+@app.get('/about')
+def about():
+    return render('about.html')
 
-@app.get('/<user>/add')
-def add_user(user):
-    report = None
-    with dataset.connect(DB_STRING) as db:
-        users = db['Users']
-        existing_user = users.find_none(nick=user)
-        if existing_user is None:
-            users.insert({'nick': user})
-            report = 'User added'
-        else:
-            report = 'User already exists'
-    return {'report': report}
+@app.get('/start')
+def start():
+    return render('start.html')
 
+@app.get('/<user>/')
+def get_profile(user):
+    user = utils.get_user(user)
+    return render('profile.html', {'user': user})
 
+@app.get('/<user>/learn')
+def user_revision(user):
+    user = utils.get_user(user)
+    return render('revision.html', {'user': user})
 
-        
+@app.get('/<level>/add')
+def add_question(level):
+    question = bottle.request.json.get('question')
+    answer = bottle.request.json.get('answer')
+    support = bottle.request.json.get('support')
 
+@app.get('/static/<path:path>')
+def static_server(path):
+    return bottle.static_file(path, root='static')
+
+# --------------MAIN
 if __name__ == '__main__':
     bottle.run(app, host='localhost', port=8080, debug=True)
--- utils.py
+++ utils.py
@@ -21,3 +21,24 @@ def render(name, data=None):
     with open(os.path.join(template_base, name), 'r') as file:
         html = file.read()
     return template(html, **data)
+
+def user_exists(user):
+    with dataset.connect(DB_STRING) as db:
+        users = db['Users']
+        existing_user = users.find_none(nick=user)
+    return existing_user is not None
+
+def create_user(user):
+    with dataset.connect(DB_STRING) as db:
+        users = db['Users']
+        users.insert({'nick': user})
+
+def get_question(level):
+    with dataset.connect(DB_STRING) as db:
+        questions = db['question']
+        q = questions.find_one(level=level)
+    return q
+
+def translate(text, target):
+    # TODO
+    return text
