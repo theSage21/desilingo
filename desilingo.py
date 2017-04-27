d2915d6f99e11efbfd96989670860138c2bf0b26
Arjoonn Sharma <arjoonn.94@gmail.com>
2017-04-27 14:39:14 +0530

    about and start page

--- desilingo.py
+++ desilingo.py
@@ -25,21 +25,11 @@ def get_profile(user):
     user = utils.get_user(user)
     return render('profile.html', {'user': user})
 
-@app.get('/<user>/learn')
-def user_revision(user):
-    user = utils.get_user(user)
-    return render('revision.html', {'user': user})
 
-@app.get('/<level>/add')
-def add_question(level):
-    question = bottle.request.json.get('question')
-    answer = bottle.request.json.get('answer')
-    support = bottle.request.json.get('support')
-
+# --------------MAIN
 @app.get('/static/<path:path>')
 def static_server(path):
     return bottle.static_file(path, root='static')
 
-# --------------MAIN
 if __name__ == '__main__':
     bottle.run(app, host='localhost', port=8080, debug=True)
